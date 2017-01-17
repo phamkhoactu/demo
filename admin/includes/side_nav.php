a4b7e27f608298b91b01ca8984f94480a481cd88
khoaphamctu <khoab1401150@student.ctu.edu.vn>
2017-01-17 23:08:20 +0700

    Upload gallery

---
+++ admin/includes/side_nav.php
@@ -0,0 +1,22 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li class="active">
+
+            <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i
+                    class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="demo" class="collapse">
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+            </ul>
+        </li>
+
+    </ul>
+</div>
